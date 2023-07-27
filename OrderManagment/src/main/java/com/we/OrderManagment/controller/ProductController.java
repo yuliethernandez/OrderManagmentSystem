637a0243ee96a77998cb6a2af1802da2860ef2f2
Yuliet Hernandez <yuliethdez2020@gmail.com>
2023-07-27 20:13:35 +0100

    Adding Html page of product

--- OrderManagment/src/main/java/com/we/OrderManagment/controller/ProductController.java
+++ OrderManagment/src/main/java/com/we/OrderManagment/controller/ProductController.java
@@ -41,86 +41,88 @@ public class ProductController {
         return "redirect:/products";
     } 
     
-    @GetMapping("supplierDetails")
+    @GetMapping("productDetails")
     public String courseProduct(Integer id, Model model) {
-        Product supplier = service.getProductByID(id);
-                
-        model.addAttribute("supplier", supplier);
-        return "supplierDetails";
+        Product product = service.getProductByID(id);
+        if(product.getTax() == true){
+            model.addAttribute("tax", true);
+        }       
+        model.addAttribute("product", product);
+        return "productDetails";
     }
     
     @GetMapping("editProduct")
     public String getEditProductById(Integer id, Model model) {
-        Product supplier;
+        Product product;
         if(id != null){
-            supplier = service.getProductByID(id);
+            product = service.getProductByID(id);
         }else{
             return "products";
         }
         
-        model.addAttribute("supplier", supplier);
+        model.addAttribute("product", product);
         
         return "editProduct";
         
     }
     
     @PostMapping("editProduct")
-    public String editProduct(@Valid Product supplier, BindingResult result, 
+    public String editProduct(@Valid Product product, BindingResult result, 
             HttpServletRequest request, Model model) {
         
         if (result.hasErrors()) {
-            Product sup = service.getProductByID(supplier.getId());
-            model.addAttribute("supplier", sup);
+            Product prod = service.getProductByID(product.getId());
+            model.addAttribute("product", prod);
                         
             return "editProduct";
         }
         
-        service.updateProduct(supplier);
+        service.updateProduct(product);
 
         return "redirect:/products";
     }
     
     @GetMapping("addProduct")
     public String addProductPage(Model model, HttpServletRequest request) {     
-        List<Product> products = service.getAllProducts();
-        model.addAttribute("products", products);
+//        List<Product> products = service.getAllProducts();
+//        model.addAttribute("products", products);
         model.addAttribute("errors", violations);
         
         return "addProduct";
     }
    
-    @PostMapping("addProduct")
-    public String addProduct(Model model, HttpServletRequest request) {   
-        Product supplier = new Product();
-        String[] productsIds = request.getParameterValues("productsId");
-        if(productsIds != null){
-            List<Product> products = new ArrayList<>();
-            for(String id : productsIds) {
-                products.add(service.getProductByID(Integer.parseInt(id)));
-            }
-            supplier.setProducts(products);
-            
-        }
-        supplier.setName(request.getParameter("name"));
-        supplier.setAddress(request.getParameter("address"));   
-        supplier.setPhonenumber(request.getParameter("phonenumber"));
-        supplier.setEmail(request.getParameter("email"));
-        supplier.setDetails(request.getParameter("details"));
+//    @PostMapping("addProduct")
+//    public String addProduct(Model model, HttpServletRequest request) {   
+//        Product product = new Product();
+//        String[] productsIds = request.getParameterValues("productsId");
+//        if(productsIds != null){
+//            List<Product> products = new ArrayList<>();
+//            for(String id : productsIds) {
+//                products.add(service.getProductByID(Integer.parseInt(id)));
+//            }
+//            supplier.setProducts(products);
+//            
+//        }
+//        supplier.setName(request.getParameter("name"));
+//        supplier.setAddress(request.getParameter("address"));   
+//        supplier.setPhonenumber(request.getParameter("phonenumber"));
+//        supplier.setEmail(request.getParameter("email"));
+//        supplier.setDetails(request.getParameter("details"));
+//
+//        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+//        violations = validate.validate(supplier);
+//
+//        if(violations.isEmpty()) {  
+//            if(product.getProducts() == null) {                 
+//                return "redirect:/products";
+//            }
+//            service.addProduct(supplier);            
+//            return "redirect:/products";
+//        }
+//                
+//        return "redirect:/addProduct";
 
-        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
-        violations = validate.validate(supplier);
-
-        if(violations.isEmpty()) {  
-            if(supplier.getProducts() == null) {                 
-                return "redirect:/products";
-            }
-            service.addProduct(supplier);            
-            return "redirect:/products";
-        }
-                
-        return "redirect:/addProduct";
-
-    }
+//    }
     
     
 }
